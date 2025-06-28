4ad926887a70697a3e0649520a3c8e1c4e6e7a64
kevin andrews <kevinandrews001@gmail.com>
2025-06-28 12:08:35 +0530

    latest push

--- client/src/pages/kadagamNext/pricingPage.jsx
+++ client/src/pages/kadagamNext/pricingPage.jsx
@@ -1,6 +1,6 @@
 // src/pages/pricing/PricingPage.jsx
 
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import paymentService from "../../services/paymentService";
